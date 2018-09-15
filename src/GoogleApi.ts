7cdc0760a30abab2458ebf53e9a69af5160e8382
Alexandre Pare <alex.pare.inc@gmail.com>
2018-09-15 19:23:33 -0400

    update

--- src/GoogleApi.ts
+++ src/GoogleApi.ts
@@ -1,7 +1,6 @@
 /// <reference path="./Ajax.ts"/>
 
-module GoogleApi {
-
+namespace GoogleApi {
   export interface Calendar {
     id: string;
     summary: string;
@@ -50,62 +49,86 @@ module GoogleApi {
     items: Event[];
   }
 
-  var rest: Rest = new Rest('https://www.googleapis.com/');
+  var rest: Rest = new Rest("https://www.googleapis.com/");
   export var accessToken: string;
 
-  function call<T>(methode: string, url: string, queryString?: { [key: string]: any }, data?: { [key: string]: any }) {
-    return authenticate()
-      .then((token) => {
-        if (queryString == null) {
-          queryString = {};
-        }
-        queryString['access_token'] = token;
-        return rest.call<T>(methode, url, queryString, data)
-      })
+  function call<T>(
+    methode: string,
+    url: string,
+    queryString?: { [key: string]: any },
+    data?: { [key: string]: any }
+  ) {
+    return authenticate().then(token => {
+      if (queryString == null) {
+        queryString = {};
+      }
+      queryString["access_token"] = token;
+      return rest.call<T>(methode, url, queryString, data);
+    });
   }
 
   export function getCalendars() {
-    return call('GET', 'calendar/v3/users/me/calendarList', {
+    return call("GET", "calendar/v3/users/me/calendarList", {
       fields: "items(id,summary)"
-    }).get<Calendar[]>('items')
+    }).get<Calendar[]>("items");
   }
 
   export function createCalendar(calendarSummary: string) {
-    return call<Calendar>('POST', 'calendar/v3/calendars', null, {
-      "summary": calendarSummary,
+    return call<Calendar>("POST", "calendar/v3/calendars", null, {
+      summary: calendarSummary
     });
   }
 
   export function getEvents(calandarId: string) {
-    return call<EventList>('GET', 'calendar/v3/calendars/' + encodeURIComponent(calandarId) + '/events', {
-      maxResults: 2500,
-      fields: "items(id, summary, extendedProperties/shared, location, start, end)",
-      sharedExtendedProperty: "UQCalendar=3"
-    }).get<Event[]>('items')
+    return call<EventList>(
+      "GET",
+      "calendar/v3/calendars/" + encodeURIComponent(calandarId) + "/events",
+      {
+        maxResults: 2500,
+        fields:
+          "items(id, summary, extendedProperties/shared, location, start, end)",
+        sharedExtendedProperty: "UQCalendar=3"
+      }
+    ).get<Event[]>("items");
   }
 
   export function createEvent(calandarId: string, event: GoogleApi.Event) {
-    return call<GoogleApi.Event>('POST', 'calendar/v3/calendars/' + encodeURIComponent(calandarId) + '/events', {
-      fields: "id, summary, extendedProperties/shared, location, start, end"
-    }, <any>event);
+    return call<GoogleApi.Event>(
+      "POST",
+      "calendar/v3/calendars/" + encodeURIComponent(calandarId) + "/events",
+      {
+        fields: "id, summary, extendedProperties/shared, location, start, end"
+      },
+      <any>event
+    );
   }
 
   export function deleteEvent(calandarId: string, event: Event) {
-    return call('DELETE', 'calendar/v3/calendars/' + encodeURIComponent(calandarId) + '/events/' + encodeURIComponent(event.id)).then(() => {
+    return call(
+      "DELETE",
+      "calendar/v3/calendars/" +
+        encodeURIComponent(calandarId) +
+        "/events/" +
+        encodeURIComponent(event.id)
+    ).then(() => {
       event.id = null;
       return event;
-    })
+    });
   }
 
   export function authenticate(prompt = false) {
     var deferred = Q.defer<string>();
-    chrome.identity.getAuthToken({ interactive: prompt }, (token) => {
-      if (token == null) {
-        deferred.reject(chrome.runtime.lastError.message);
-      } else {
-        deferred.resolve(token);
-      }
-    });
+    try {
+      chrome.identity.getAuthToken({ interactive: prompt }, token => {
+        if (token == null) {
+          deferred.reject(chrome.runtime.lastError.message);
+        } else {
+          deferred.resolve(token);
+        }
+      });
+    } catch (e) {
+      deferred.reject(e);
+    }
     return deferred.promise;
   }
 }
